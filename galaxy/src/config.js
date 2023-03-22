50c640cfe838252f8c1e073ef5b6ea93c9cab42b
shubh@3ai <shubh@thirdai.com>
2023-03-22 11:20:01 +0000

    Save config, because we and vscode are idiots

--- galaxy/src/config.js
+++ galaxy/src/config.js
@@ -1,10 +1,4 @@
 export default {
-    dataUrl: '//localhost:8081/',
-    apiUrl: 'http://localhost:8000/predict'
-
-
-    /*
-    dataUrl: '/galaxy/data/',
-    apiUrl: '/cold-start-api/predict'
-    */
+    dataUrl: '/',
+    apiUrl: '/predict'
 };
