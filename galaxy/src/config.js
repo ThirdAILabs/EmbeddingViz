3214ead649633d3c3684b2365938e47ed50adbc2
shubh@3ai <shubh@thirdai.com>
2023-04-05 07:35:46 +0000

    Update js config

--- galaxy/src/config.js
+++ galaxy/src/config.js
@@ -1,8 +1,8 @@
 export default {
-    dataUrl: '/biblemodel/', 
-    apiUrl: '/biblemodel/predict',
-    target_name: 'ID', 
-    strong_column_names: ['Passage'], 
-    description: 'Bible hehe', 
-    name: 'Using model inferencing', 
+    dataUrl: '/food/',
+    apiUrl: '/food/predict',
+    target_name: 'RecipeId',
+    strong_column_names: ['Name'],
+    description: 'Food Recipes Search',
+    name: 'Cold Start Model for food recipes scraped from food.com. This model returns the recipes that matches with your queries the best.',
 };
